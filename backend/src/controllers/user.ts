25b8c8a9c78e59741ee613dd0cccdfa8b0f5eecc
Catharina Anggono <79496983+catharinaanggono@users.noreply.github.com>
2023-09-19 00:18:35 +0800

    Implemented error checking for existing username

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -7,17 +7,18 @@ export const getAuthenticatedUser: RequestHandler = async (req, res, next) => {
   const authenticatedUserId = req.session.userId;
 
   try {
-    if(!authenticatedUserId){
-      throw createHttpError(401, "User not authenticated")
+    if (!authenticatedUserId) {
+      throw createHttpError(401, "User not authenticated");
     }
 
-    const user = await UserModel.findById(authenticatedUserId).select("+email").exec()
-    res.status(200).json(user)
-
+    const user = await UserModel.findById(authenticatedUserId)
+      .select("+email")
+      .exec();
+    res.status(200).json(user);
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 export const getUser: RequestHandler = async (req, res, next) => {
   const username = req.params.username;
@@ -78,6 +79,12 @@ export const createUser: RequestHandler<
       throw createHttpError(400, "Missing parameters!");
     }
 
+    const existUser = await UserModel.findOne({ username: username }).exec();
+
+    if (existUser) {
+      throw createHttpError(400, "User already created!");
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const newUser = await UserModel.create({
@@ -104,46 +111,51 @@ export const createUser: RequestHandler<
 };
 
 interface LoginBody {
-  username?: string,
-  password?: string,
+  username?: string;
+  password?: string;
 }
 
-export const login: RequestHandler<unknown, unknown, LoginBody, unknown> = async (req, res, next) => {
+export const login: RequestHandler<
+  unknown,
+  unknown,
+  LoginBody,
+  unknown
+> = async (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
 
-  try{
-    if(!username || !password){
-      throw createHttpError(400, "Parameters missin")
+  try {
+    if (!username || !password) {
+      throw createHttpError(400, "Parameters missing");
     }
 
-    const user = await UserModel.findOne({username: username}).select("+password +email").exec();
+    const user = await UserModel.findOne({ username: username })
+      .select("+password +email")
+      .exec();
 
-    if(!user){
+    if (!user) {
       throw createHttpError(401, "Invalid credentials");
     }
 
     const passwordMatch = await bcrypt.compare(password, user.password);
 
-
-    if(!passwordMatch){
-      throw createHttpError(401, "Invalid credentials")
+    if (!passwordMatch) {
+      throw createHttpError(401, "Invalid credentials");
     }
 
     req.session.userId = user._id;
     res.status(201).json(user);
-
-  }catch(error){
-    next(error)
+  } catch (error) {
+    next(error);
   }
-}
+};
 
 export const logout: RequestHandler = async (req, res, next) => {
-  req.session.destroy(error => {
-    if(error){
-      next(error)
-    }else{
-      res.sendStatus(200)
+  req.session.destroy((error) => {
+    if (error) {
+      next(error);
+    } else {
+      res.sendStatus(200);
     }
-  })
-}
+  });
+};
