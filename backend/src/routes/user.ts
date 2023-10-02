53caf84443d9fe6fc377e5845eecca60ea01825f
Rivaldi0097 <rivaldi1997@hotmail.com>
2023-10-03 02:33:16 +0800

    Update user.ts

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,6 +2,7 @@ import express from "express";
 import * as UserController from "../controllers/user";
 
 const router = express.Router();
+var cors = require("cors");
 
 router.get("/", UserController.getAuthenticatedUser);
 
@@ -9,7 +10,7 @@ router.get("/:username", UserController.getUser);
 
 router.post("/", UserController.createUser);
 
-router.post("/login", UserController.login);
+router.post("/login",cors(), UserController.login);
 
 router.post("/logout", UserController.logout);
 
