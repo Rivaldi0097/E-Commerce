582cf777d79ea1a9d436ce66000117ccaf3a8fc2
Rivaldi0097 <rivaldi1997@hotmail.com>
2023-10-03 02:37:05 +0800

    fixing

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,7 +1,11 @@
 import express from "express";
 import * as UserController from "../controllers/user";
 
+var app = express();
 const router = express.Router();
+var cors = require("cors");
+
+app.options("*", cors());
 
 router.get("/", UserController.getAuthenticatedUser);
 
