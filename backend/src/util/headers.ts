bb8cb31190233357e95d0be10bf04c3d7e4b13a3
Rivaldi0097 <rivaldi1997@hotmail.com>
2023-10-03 00:13:09 +0800

    Update headers.ts

--- backend/src/util/headers.ts
+++ backend/src/util/headers.ts
@@ -2,7 +2,7 @@ const headers = (req: any, res: any, next: any) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Credentials", true);
     res.setHeader("Access-Control-Allow-Methods", 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    res.setHeader("Access-Control-Allow-Headers",  "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 	next()
 }
 
