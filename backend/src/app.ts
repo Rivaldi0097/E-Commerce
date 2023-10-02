fbcb31daf53141743ed390ce81383286843abacd
Rivaldi0097 <rivaldi1997@hotmail.com>
2023-10-02 23:32:08 +0800

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -57,10 +57,6 @@ app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
     statusCode = error.status;
     errorMessage = error.message;
   }
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   res.status(statusCode).json({ error: errorMessage });
   
 });
