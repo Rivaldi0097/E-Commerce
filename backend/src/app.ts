e1741057ee65bdc6a0c82062fb5fee92151eb723
Rivaldi0097 <rivaldi1997@hotmail.com>
2023-10-03 00:32:19 +0800

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -15,7 +15,9 @@ import MongoStore from "connect-mongo";
 const cors = require("cors");
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 
 app.options("*", cors);
 
@@ -57,10 +59,7 @@ app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
     statusCode = error.status;
     errorMessage = error.message;
   }
-  res.setHeader("Access-Control-Allow-Origin", "localhost:3000");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+
   res.status(statusCode).json({ error: errorMessage });
   
 });
