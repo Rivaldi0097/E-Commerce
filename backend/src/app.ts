c9241fafbe622c0941e99f1c2814c57425bcb275
Rivaldi0097 <rivaldi1997@hotmail.com>
2023-10-03 14:38:54 +0800

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -16,7 +16,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({
-  origin:['http://localhost:3000'],
+  origin:['http://localhost:3000, https://e-commerce-frontend-4kcc69md1-rivaldi0097.vercel.app/'],
   Credentials: true,
   allowedHeaders: ['Content-Type']
 }));
