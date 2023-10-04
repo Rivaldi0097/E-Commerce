3c01d9f4a73387b048fe50b8c01603e3b4461e4d
Rivaldi0097 <rivaldi1997@hotmail.com>
2023-10-04 23:59:02 +0800

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -19,7 +19,7 @@ const cookieParser = require('cookie-parser')
 var whitelist = ['http://localhost:3000', 'https://e-commerce-frontend-rivaldi0097.vercel.app', 'https://e-commerce-frontend-git-main-rivaldi0097.vercel.app']
 
 app.use(cors({
-  origin: whitelist,
+  origin: 'https://e-commerce-frontend-rivaldi0097.vercel.app',
   credentials: true,
   allowedHeaders: ['Content-Type']
 }));
